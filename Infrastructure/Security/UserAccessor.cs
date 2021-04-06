93dcf9d595f0d87d600bf39285e5c3000a30203e
Francis Decena <francis.decena@ips-securex.com>
2021-04-06 15:42:09 +0800

    Commit implementing JwtGenerator and UserAccessor in Infrastructure

--- Infrastructure/Security/UserAccessor.cs
+++ Infrastructure/Security/UserAccessor.cs
@@ -1,7 +1,24 @@
+using Application.Interfaces;
+using Microsoft.AspNetCore.Http;
+using System.Linq;
+using System.Security.Claims;
+
 namespace Infrastructure.Security
 {
-    public class UserAccessor
+    public class UserAccessor : IUserAccessor
     {
+        private readonly IHttpContextAccessor _httpContextAccessor;
         
+        public UserAccessor(IHttpContextAccessor httpContextAccessor)
+        {
+            _httpContextAccessor = httpContextAccessor;
+        }
+
+        public string GetCurrentUsername()
+        {
+            var userName = _httpContextAccessor.HttpContext.User?.Claims?.FirstOrDefault(x => x.Type == ClaimTypes.NameIdentifier)?.Value;
+
+            return userName;
+        }
     }
 }
