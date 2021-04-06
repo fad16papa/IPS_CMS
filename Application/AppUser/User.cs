e0402f9a95c741098609511551146bea64ec1cf4
Francis Decena <francis.decena@ips-securex.com>
2021-04-06 14:41:40 +0800

    Commit adding dependency packages in Application

---
+++ Application/AppUser/User.cs
@@ -0,0 +1,11 @@
+namespace Application.AppUser
+{
+    public class User
+    {
+        public string DisplayName { get; set; }
+        public string Token { get; set; }
+        public string UserName { get; set; }
+        public string Email { get; set; }
+        public string Id { get; set; }
+    }
+}
