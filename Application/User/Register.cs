d91a11316d0fbc4393e8942aed190d1af85c89d2
Francis Decena <francis.decena@ips-securex.com>
2021-04-06 15:47:00 +0800

    Commit done creating the register for AppUser

--- Application/User/Register.cs
+++ Application/User/Register.cs
@@ -3,6 +3,7 @@ using System.Net;
 using System.Threading;
 using System.Threading.Tasks;
 using Application.Errors;
+using Application.Interfaces;
 using Domain;
 using FluentValidation;
 using MediatR;
@@ -25,21 +26,25 @@ namespace Application.User
         public class Handler : IRequestHandler<Command, User>
         {
             private readonly DataContext _context;
+            private readonly UserManager<AppUser> _userManager;
+            private readonly IJwtGenerator _jwtGenerator;
 
-            public Handler(DataContext context, UserManager<AppUser> userManager)
+            public Handler(DataContext context, UserManager<AppUser> userManager, IJwtGenerator jwtGenerator)
             {
+                _jwtGenerator = jwtGenerator;
+                _userManager = userManager;
                 _context = context;
             }
 
             public async Task<User> Handle(Command request, CancellationToken cancellationToken)
             {
                 //logic goes here
-                if(await _context.Users.AnyAsync(x => x.Email == request.Email))
+                if (await _context.Users.AnyAsync(x => x.Email == request.Email))
                 {
                     throw new RestException(HttpStatusCode.Conflict, string.Format($"Email {request.Email} already exist."));
                 }
-                
-                if(await _context.Users.AnyAsync(x => x.UserName == request.UserName))
+
+                if (await _context.Users.AnyAsync(x => x.UserName == request.UserName))
                 {
                     throw new RestException(HttpStatusCode.Conflict, string.Format($"Email {request.UserName} already exist."));
                 }
@@ -51,11 +56,16 @@ namespace Application.User
                     UserName = request.UserName,
                 };
 
-                var success = await _context.SaveChangesAsync() > 0;
+                var result = await _userManager.CreateAsync(user, request.Password);
 
-                if (success)
+                if (result.Succeeded)
                 {
-                    return new User() { };
+                    return new User
+                    {
+                        DisplayName = user.DisplayName,
+                        Token = _jwtGenerator.CreateToken(user),
+                        UserName = user.UserName
+                    };
                 }
                 else
                 {
