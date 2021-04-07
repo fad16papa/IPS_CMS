9a0def2a504a33478e2409929566a12d016a2e6f
Francis Decena <fad16papa@gmail.com>
2021-04-07 14:18:57 +0800

    Commit Added CreatePosition for Position Application

---
+++ Application/Positions/CreatePosition.cs
@@ -0,0 +1,57 @@
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+using Domain;
+using MediatR;
+using Persistence;
+
+namespace Application.Positions
+{
+    public class CreatePosition
+    {
+        public class CommandCreatePosition : IRequest
+        {
+            public Guid Id { get; set; }
+            public string Name { get; set; }
+            public string Description { get; set; }
+            public DateTime DateCreated { get; set; }
+            public bool IsEnable { get; set; }
+        }
+
+        public class Handler : IRequestHandler<CommandCreatePosition>
+        {
+            private readonly DataContext _context;
+
+            public Handler(DataContext context)
+            {
+                _context = context;
+            }
+
+            public async Task<Unit> Handle(CommandCreatePosition request, CancellationToken cancellationToken)
+            {
+                //logic goes here
+                var position = new Position
+                {
+                    Id = request.Id,
+                    Name = request.Name,
+                    Description = request.Description,
+                    DateCreated = request.DateCreated,
+                    IsEnable = request.IsEnable
+                };
+
+                _context.Position.Add(position);
+
+                var success = await _context.SaveChangesAsync() > 0;
+
+                if (success)
+                {
+                    return Unit.Value;
+                }
+                else
+                {
+                    throw new Exception("Problem saving changes");
+                }
+            }
+        }
+    }
+}
