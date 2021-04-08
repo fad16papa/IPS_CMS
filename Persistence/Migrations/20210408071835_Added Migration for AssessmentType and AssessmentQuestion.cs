6135ea0e1772d519a0a508dfdedea5b3c4ec8be1
Francis Decena <fad16papa@gmail.com>
2021-04-08 15:21:04 +0800

    Commit added migrations for AssessmentType and AssessmentQuestion

---
+++ Persistence/Migrations/20210408071835_Added Migration for AssessmentType and AssessmentQuestion.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Persistence.Migrations
+{
+    public partial class AddedMigrationforAssessmentTypeandAssessmentQuestion : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "AssessmentType",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    DateCreated = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    IsEnable = table.Column<bool>(type: "bit", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_AssessmentType", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "AssessmentQuestion",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Question = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    AssessmentTypeId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Points = table.Column<int>(type: "int", nullable: false),
+                    DateCreated = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    IsEnable = table.Column<bool>(type: "bit", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_AssessmentQuestion", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_AssessmentQuestion_AssessmentType_AssessmentTypeId",
+                        column: x => x.AssessmentTypeId,
+                        principalTable: "AssessmentType",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_AssessmentQuestion_AssessmentTypeId",
+                table: "AssessmentQuestion",
+                column: "AssessmentTypeId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "AssessmentQuestion");
+
+            migrationBuilder.DropTable(
+                name: "AssessmentType");
+        }
+    }
+}
