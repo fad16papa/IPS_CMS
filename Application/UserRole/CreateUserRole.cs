c0c4ebe050dcdc6ba78bed5df4c3b52e302a5e2c
Francis Decena <fad16papa@gmail.com>
2021-04-12 12:23:21 +0800

    Commit Added CreateUserRole in UserRole Application

---
+++ Application/UserRole/CreateUserRole.cs
@@ -0,0 +1,51 @@
+using System;
+using System.Net;
+using System.Threading;
+using System.Threading.Tasks;
+using Application.Errors;
+using Domain;
+using MediatR;
+using Microsoft.AspNetCore.Identity;
+
+namespace Application.UserRole
+{
+    public class CreateUserRole
+    {
+        public class CommandCreateUserRole : IRequest
+        {
+            public AppUserRole AppUserRole { get; set; }
+        }
+
+        public class Handler : IRequestHandler<CommandCreateUserRole>
+        {
+            private readonly RoleManager<AppUserRole> _roleManager;
+
+            public Handler(RoleManager<AppUserRole> roleManager)
+            {
+                _roleManager = roleManager;
+            }
+
+            public async Task<Unit> Handle(CommandCreateUserRole request, CancellationToken cancellationToken)
+            {
+                //logic goes here
+                bool check = await _roleManager.RoleExistsAsync(request.AppUserRole.Name);
+
+                if (check)
+                {
+                    throw new RestException(HttpStatusCode.Conflict, string.Format("The role name{0} is already created", request.AppUserRole.Name));
+                }
+
+                var result = await _roleManager.CreateAsync(request.AppUserRole);
+
+                if (result.Succeeded)
+                {
+                    return Unit.Value;
+                }
+                else
+                {
+                    throw new Exception("Problem saving changes");
+                }
+            }
+        }
+    }
+}
--- Domain/AppUserRole.cs
+++ Domain/AppUserRole.cs
@@ -1,9 +1,10 @@
+using System;
 using Microsoft.AspNetCore.Identity;
 
 namespace Domain
 {
     public class AppUserRole : IdentityRole
     {
-
+        public DateTime DateCreated { get; set; }
     }
 }
